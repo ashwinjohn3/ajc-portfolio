668a36b024becf2dfe276ec23ca1adb19c7bfc54
Ashwin Chempolil <chempolil.a@husky.neu.edu>
2024-11-04 20:50:38 -0500

    Revert "Merge pull request #1 from ashwinjohn3/nav-bar-development"
    This reverts commit 55cc7243c24a5d25cfa16ae0816f0e5fbc96d34e, reversing
    changes made to 3d7780bb57a6b120752894794be24334ba63939a.

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,36 @@
-import Link from 'next/link';
 import { FaGithub, FaLinkedin, FaCameraRetro } from 'react-icons/fa6';
 
 
 export default function Home() {
   const fontSize = 24;
   return (
-    <div>
-      <nav className='flex flex-col items-center border-b mb-5 py-5 bg-secondary'>
-        <div className='flex justify-between max-w-6xl w-full'>
-          <div className='flex gap-5 items-center'>
-            <Link href="/">
-              Home
-            </Link>
-            <Link href="/abouts">
-              About
-            </Link>
-            <Link href="/projects">
-              Projects
-            </Link>
-            <Link href="/">
-              Blog
-            </Link>
-          </div>
-        </div>
-      </nav>
-      <div className="min-h-screen flex items-center justify-center text-center">
-        <main>
-          <h1 className="font-semibold text-4xl">ashwin john chempolil</h1>
-          <h3 className='flex gap-4 justify-center mt-4 text-lg'>
-            <a 
-              href="https://aws.amazon.com/ram/" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="relative inline-block group"
-            >
-              sde 2 at aws ram
-              <span className="hidden group-hover:inline absolute"> 🐏</span>
-              {/* <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 bg-[length:200%_100%] animate-gradient"></span> */}
-            </a>
-          </h3>
-          <h3 className="flex gap-4 justify-center mt-4">
-            <a href="https://www.linkedin.com/in/ashwinjohn3/" target="_blank" rel="noopener noreferrer" >
-              <FaLinkedin size={fontSize} />
-            </a>
-            <a href="https://vsco.co/thelegendashw1n/gallery" target="_blank" rel="noopener noreferrer">
-              <FaCameraRetro size={fontSize} />
-            </a>
-            <a href="https://github.com/ashwinjohn3" target="_blank" rel="noopener noreferrer">
-              <FaGithub size={fontSize} />
-            </a>
-          </h3>
-        </main>
-      </div>
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <main>
+        <h1 className="font-semibold text-4xl">ashwin john chempolil</h1>
+        <h3 className='flex gap-4 justify-center mt-4 text-lg'>
+          <a 
+            href="https://aws.amazon.com/ram/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="relative inline-block group"
+          >
+            sde 2 at aws ram
+            <span className="hidden group-hover:inline absolute"> 🐏</span>
+            {/* <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 bg-[length:200%_100%] animate-gradient"></span> */}
+          </a>
+        </h3>
+        <h3 className="flex gap-4 justify-center mt-4">
+          <a href="https://www.linkedin.com/in/ashwinjohn3/" target="_blank" rel="noopener noreferrer" >
+            <FaLinkedin size={fontSize} />
+          </a>
+          <a href="https://vsco.co/thelegendashw1n/gallery" target="_blank" rel="noopener noreferrer">
+            <FaCameraRetro size={fontSize} />
+          </a>
+          <a href="https://github.com/ashwinjohn3" target="_blank" rel="noopener noreferrer">
+            <FaGithub size={fontSize} />
+          </a>
+        </h3>
+      </main>
     </div>
   );
 }
